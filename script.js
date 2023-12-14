a9ba96a47b04efe9e85d615a171f8ab77ec1da62
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-14 15:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+  const coins = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0
+  };
 
+  while (amount > 0) {
+    if (amount >= 25) {
+      coins.q++;
+      amount -= 25;
+    } else if (amount >= 10) {
+      coins.d++;
+      amount -= 10;
+    } else if (amount >= 5) {
+      coins.n++;
+      amount -= 5;
+    } else {
+      coins.p += amount;
+      amount = 0;
+    }
+  }
+
+  return coins;
+}
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
